eb5fe097c0534d81015eccb56e8a7a0b9a049dac
maksimmanzhai <bender23max@gmail.com>
2019-01-11 22:22:50 +0300

    Add terms and about views

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/contacts'
   get 'home/index'
-  
-  resources :terms
-  # resources :about
-  get 'about', action: :index, controller: 'about'
-  resources :articles
-  resource :contacts, only: [:new, :create], path_names: { :new => ''}
+  get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
 
+  resource :contacts, only: [:new, :create], path_names: { :new => ''}
+  resources :articles
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

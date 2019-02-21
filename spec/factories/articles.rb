bf1791236b97424f21f514c2971726c82f4f8e36
maksimmanzhai <bender23max@gmail.com>
2019-02-21 21:36:53 +0300

    Add comment factory

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,6 +1,13 @@
 FactoryGirl.define do
   factory :article do
-    title "Arrticle title"
+    title "Article title"
     text "Article text"
+
+    # create factory article_with_comments
+    factory :article_with_comments do
+      after :create do |article, evaluator|
+        create_list :comment, 3,  article: article
+      end
+    end
   end
 end
